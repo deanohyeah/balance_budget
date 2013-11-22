377d4feffb1ca47d595a8e1c6478a5e6b96b7a04
Dean Kramer <deanjkramer@gmail.com>
2013-11-21 22:45:19 -0800

    added sections to category in api

--- balance_budget/api.py
+++ balance_budget/api.py
@@ -1,13 +1,20 @@
-from tastypie.cache import SimpleCache
 from tastypie import fields
 from tastypie.resources import ModelResource,ALL_WITH_RELATIONS
 from .models import *
-from django.http import HttpResponse
 #test url
 #/elections/api/v1/race/?format=json&limit=300&locations__locations__county__in=Snohomish,King&race_status=featured
 
 
+
+class SectionResource(ModelResource):
+    class Meta:
+        queryset = Section.objects.all()
+        resource_name = 'sections'
+
+
 class CategoryResource(ModelResource):
+
+    sections = fields.ToManyField(SectionResource, 'sections',null=True,full = True)
     class Meta:
         queryset = Category.objects.all()
         resource_name = 'category'
--- balance_budget/models.py
+++ balance_budget/models.py
@@ -1,9 +1,5 @@
-import threading
 from django.db import models
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.core.management import call_command
+
 
 
 
@@ -23,20 +19,10 @@ class Section(models.Model):
     name = models.CharField(max_length=200, blank=True)
     info = models.TextField(max_length=500, blank=True)
     value = models.IntegerField()
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category,related_name='sections')
     taxFlag = models.NullBooleanField()
     impact = models.TextField(max_length=500, blank=True)
     increaseDef = models.NullBooleanField()
-    
-    def save(self, *args, **kwargs):
-        """
-        Override save method to call django bakery commands.
-        This must happen after the item is actually saved in the database.
-        """
-        super(Section, self).save()
-        if settings.BAKERY_ACTIVATED:
-            call_command('build')
-            call_command('publish')
     
     def __unicode__(self):
         return self.name  
