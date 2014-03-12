7cd0fb429f1dc131b237ecc6e7527edeb375028a
Dean Kramer <deanjkramer@gmail.com>
2014-03-11 21:56:01 -0700

    Budget resize for small windows creates wrong benchmark heights fix #21

--- balance_budget/static/scripts/models/item.js
+++ balance_budget/static/scripts/models/item.js
@@ -11,22 +11,21 @@ define(function(require, exports, module)
             initialize: function(){
                 this.deficit = 0;
                 this.maxValue = 2000+this.deficit;
+                var budgetContainer = $('.budget_container');
+                this.containerHeight = budgetContainer.height() || 500;
             },
 
             getPixelHeight: function(value){ 
                 var 
-                budgetContainer = $('.budget_container'),
-                containerHeight = budgetContainer.height(),
-                pixel = value*containerHeight,
+                pixel = value*this.containerHeight,
                 pixel = pixel/this.maxValue;
                 return pixel
             },
 
             getPixelHeightBenchmarks: function(value){
                 var 
-                containerHeight = 500,
                 value           = value + this.deficit, //offsets the deficit
-                pixel           = value * containerHeight,
+                pixel           = value * this.containerHeight,
                 pixel           = pixel / this.maxValue;
                 return pixel;
             },
