7fbb186dbbef913e3df24a3f88a195c7679c75e5
Dean Kramer <deanjkramer@gmail.com>
2014-03-24 19:38:35 -0700

    added iframe support

--- balance_budget/views.py
+++ balance_budget/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response
 
+@xframe_options_exempt
 def index(request):
     return render(request, 'balance_budget/index.html')
