7bc870ecc0773cb20e0e02709fd008da726fe60b
Dean Kramer <deanjkramer@gmail.com>
2014-03-24 19:45:14 -0700

    actually added iframe support

--- balance_budget/views.py
+++ balance_budget/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.clickjacking import xframe_options_exempt
 from django.template import RequestContext
 from django.shortcuts import render, render_to_response
 
